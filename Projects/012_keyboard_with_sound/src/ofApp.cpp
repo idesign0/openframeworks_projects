360f37f22b4b83c60135216d686fbb9f75f5b160
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-07-18 22:36:01 +0530

    012-keyboard-with-sound

--- Projects/012_keyboard_with_sound/src/ofApp.cpp
+++ Projects/012_keyboard_with_sound/src/ofApp.cpp
@@ -2,7 +2,8 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	ac.load("ac.mp3");
+	happy.load("happy.mp3");
 }
 
 //--------------------------------------------------------------
@@ -17,12 +18,38 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	switch (key)	
+	{
+	case 'a': 
+		ac.play();
+		break;
+	case '+':
+		ac.setVolume(1);
+		break;
+	case '-':
+		ac.setVolume(0.2);
+		break;
+	case 'o':
+		ac.setSpeed(0.3);
+		break;
+	case 'e':
+		ac.setSpeed(-2.0);
+		break;
+	default:
+		break;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+	switch (key)
+	{
+	case 'u':
+		ac.stop();
+		break;
+	default:
+		break;
+	}
 }
 
 //--------------------------------------------------------------
