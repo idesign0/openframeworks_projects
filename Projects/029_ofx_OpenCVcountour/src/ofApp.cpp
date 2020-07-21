0aea745eb08421323f7de64a20c836b4b3f2b8cf
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-07-22 01:13:35 +0530

    029_ofx_Opencvcontourfinder

---
+++ Projects/029_ofx_OpenCVcountour/src/ofApp.cpp
@@ -0,0 +1,106 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	camera.setDeviceID(1);
+	camera.setup(640*1.2,480*1.2);
+
+	color.allocate(camera.getWidth(), camera.getHeight());
+	gray.allocate(camera.getWidth(), camera.getHeight());
+	background.allocate(camera.getWidth(), camera.getHeight());
+	difference.allocate(camera.getWidth(), camera.getHeight());
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	camera.update();
+	if (camera.isFrameNew()) {
+		color.setFromPixels(camera.getPixels());
+		gray = color;
+
+		if (learn) {
+			background = gray;
+			learn = false;
+		}
+
+		difference.absDiff(background, gray);
+		difference.threshold(threshold);
+
+		contour.findContours(difference, 10, camera.getWidth()*camera.getHeight(), 10, true);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	color.draw(0, 0);
+	contour.draw(0, 0);
+	//gray.draw(480, 0);
+	difference.draw(480*1.2, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	switch (key)
+	{
+	case OF_KEY_UP :
+		threshold++;
+		break;
+
+	case OF_KEY_DOWN:
+		threshold--;
+		break;
+
+	default:
+		break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
